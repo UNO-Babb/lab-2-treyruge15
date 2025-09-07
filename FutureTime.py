9112424afa516076487031406b36e420acdbd69f
treyruge15 <treyruge15@gmail.com>
2025-09-07 21:20:06 +0000

    Lab 2  - Trey Ruge

--- FutureTime.py
+++ FutureTime.py
@@ -9,16 +9,27 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  print("Future Time Calculator")
+  moreMins = int(input("Enter how many minutes will elapse: "))
+  moreHours = int(input("Enter how many hours will elapse: "))
+  
+  futureMins = (currentMinute + moreMins) % 60
+  extraHour = (currentMinute + moreMins) // 60
+  futureHour = (currentHour + moreHours + extraHour) % 24
 
   #Calculate the time after the user-supplied time has passed.
+  futureTime = ("futureHour + futureMins")
+
+  print("The time will be: ")
+  print(futureHour, futureMins)
+
 
   #Do not use any if statements in calculating the time.
 
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,20 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
+Question = input("Enter a Yes or No question: ")
 
+answers = ["No way Jose.", "Only time will tell.", "Without a doubt", "Yes", "Yes, 100% yes.", "It is destined to happen.",
+           "That's a great question, :).", "Not a chance", "Try again", "It has been and always will be", "No", 
+           "Never goning to happen", "God is still trying to figure that one out", "I would bet the house against it", 
+           "My grandma has a better chance of walking right out of her grave", "I've seen the future and it's happen", 
+           "60 percent of the time, it'll be 100 percent true", "I believe so", "I'd say so", "Certainly so, right?"]
   #Answer question randomly with one of the options from your earlier list.
+length = len(answers)
+r=random.random() * length
+r=int(r)
 
+response = answers[r]
+print(response)
 
 if __name__ == '__main__':
   main()
